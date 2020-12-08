af0edf67f903ee9655e0d71a84a118c98e6932c0
shirgoldberger <shirgoldberger@gmail.com>
2020-12-08 18:58:17 +0200

    finish

--- ex2/server.py
+++ ex2/server.py
@@ -6,16 +6,11 @@ MESSAGE200 = "HTTP/1.1 200 OK\r\n"
 MESSAGE404 = "HTTP/1.1 404 Not Found\r\nConnection: close\r\n\r\n"
 MESSAGE301 = "HTTP/1.1 301 Moved Permanently\r\nConnection: close\r\nLocation: /result.html\r\n\r\n"
 INDEX = "/index.html"
-RESULT = "/result.html"
+REDIRECT = "/redirect"
 CONNECTION = "Connection: "
 LEN = "Content-Length: "
 TWO_LINES = "\r\n\r\n"
 ONE_LINE = "\r\n"
-
-
-def reset_socket(client_socket):
-    client_socket.close()
-    print('Client disconnected')
 
 
 def take_file_name(data_array):
@@ -30,9 +25,8 @@ def take_file_name(data_array):
 def check_file_name(file_name):
     if file_name == "/":
         file_name = INDEX
-    if file_name == "/redirect":
-        file_name = RESULT
-    file_name = "files" + file_name
+    if file_name != REDIRECT:
+        file_name = "files" + file_name
     return file_name
 
 
@@ -51,7 +45,6 @@ def main():
     server_socket.listen(1)
     while True:
         client_socket, client_address = server_socket.accept()
-        print('Connection from: ', client_address)
         client_socket.settimeout(1.0)
         data = ""
         while True:
@@ -59,7 +52,7 @@ def main():
                 data_byte = client_socket.recv(100)
                 if not data_byte:
                     if data == "":
-                        reset_socket(client_socket)
+                        client_socket.close()
                         break
                 else:
                     data += data_byte.decode()
@@ -70,54 +63,49 @@ def main():
                     continue
                 real_data = data[0:index]
                 data = data[(index + 4):]
-                print('Received: ', data_byte.decode())
+                print(data_byte.decode())
                 if real_data == "":
-                    reset_socket(client_socket)
+                    client_socket.close()
                     break
                 data_array = real_data.split(ONE_LINE)
                 # save status connection
                 connection = take_status_connection(data_array)
                 if connection == "":
-                    reset_socket(client_socket)
+                    client_socket.close()
                     break
                 file_name = take_file_name(data_array)
                 file_name = check_file_name(file_name)
-                print(f'file name: {file_name}')
                 # the file does not exist
                 if not os.path.isfile(file_name):
                     client_socket.send(MESSAGE404.encode())
                     if connection == "close":
-                        reset_socket(client_socket)
+                        client_socket.close()
                         break
                     else:
                         continue
                 # the file is an image or an icon
-                if file_name.endswith(("jpg", "ico")):
+                if file_name == REDIRECT:
+                    message = MESSAGE301.encode()
+                elif file_name.endswith(("jpg", "ico")):
                     with open(file_name, "rb") as file:
                         content = file.read()
                         length = os.stat(file_name).st_size
                         message = (MESSAGE200 + CONNECTION + connection + ONE_LINE + LEN + str(
                             length) + TWO_LINES).encode()
+                        message += content
                 else:
                     with open(file_name, "r") as file:
                         content = file.read().encode()
                         length = len(content)
-                        message = (MESSAGE200 + CONNECTION + connection + "\r\n" + LEN
-                                   + str(length) + "\r\n\r\n").encode()
-                if file_name == "files/result.html":
-                    message = MESSAGE301.encode()
-                message += content
+                        message = (MESSAGE200 + CONNECTION + connection + ONE_LINE + LEN
+                                   + str(length) + TWO_LINES).encode()
+                        message += content
                 client_socket.send(message)
                 if connection == "close":
-                    reset_socket(client_socket)
+                    client_socket.close()
                     break
             except socket.timeout:
-                print("timeout")
-                reset_socket(client_socket)
-                break
-            except IndexError:
-                print("index error")
-                reset_socket(client_socket)
+                client_socket.close()
                 break
 
 
